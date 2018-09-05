032b4d6fef6077c1124d48aa2477bea37119f659
Charlie Cao <charlie.cao@leonardo.com>
2018-09-05 16:10:51 -0400

    updated

--- bin/mapAMI.sh
+++ bin/mapAMI.sh
@@ -9,6 +9,7 @@ echo "Searching for image where name begins with $1"
 regions=$(aws ec2 describe-regions --output text --query 'Regions[*].RegionName')
 for region in $regions; do
     (
-     echo -e "$region : \n$( aws ec2 describe-images --owners self amazon --region $region|grep -B 29 "\<${prefix_str}.*GP2\>"|grep 'ImageId'|head -n $ami_number)"|sed "s/,/  #$prefix_str/g"
+    echo -e "$region:" \\n \"AMALINUX\" : $(aws ec2 describe-images --region $region --owners amazon --filters "Name=description,Values=$prefix_str*x86_64 ECS HVM GP2" "Name=virtualization-type,Values=hvm" "Name=root-device-type,Values=ebs" "Name=state,Values=available"|jq -c '.Images[] | {ImageId, Name, Description,VirtualizationType,State}'|jq .ImageId|head -n $ami_number)
+     #echo -e "$region : \n$( aws ec2 describe-images --owners self amazon --region $region|grep -B 29 "\<${prefix_str}.*GP2\>"|grep 'ImageId'|head -n $ami_number)"|sed "s/,/  #$prefix_str/g"
     ) 
 done
