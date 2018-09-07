fe863c7a7ed3f613213dbe15b91af6fe35086387
jianyuan cao <jianyuan.cao@gmail.com>
2018-09-07 18:43:12 -0400

    update

--- bin/mapAMI.sh
+++ bin/mapAMI.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [ "$#" -ne 2 ];then
   echo "[Error ] Missing parameter: $0 prefix ami_number"
+  echo "[Error ] Missing parameter: $0 'Amazon Linux AMI 2017.03' 1"
   exit 1
 fi
 prefix_str=$1
@@ -9,7 +10,6 @@ echo "Searching for image where name begins with $1"
 regions=$(aws ec2 describe-regions --output text --query 'Regions[*].RegionName')
 for region in $regions; do
     (
-    echo -e "$region:" \\n \"AMALINUX\" : $(aws ec2 describe-images --region $region --owners amazon --filters "Name=description,Values=$prefix_str*x86_64 ECS HVM GP2" "Name=virtualization-type,Values=hvm" "Name=root-device-type,Values=ebs" "Name=state,Values=available"|jq -c '.Images[] | {ImageId, Name, Description,VirtualizationType,State}'|jq .ImageId|head -n $ami_number)
-     #echo -e "$region : \n$( aws ec2 describe-images --owners self amazon --region $region|grep -B 29 "\<${prefix_str}.*GP2\>"|grep 'ImageId'|head -n $ami_number)"|sed "s/,/  #$prefix_str/g"
+    echo -e "$region:"\\n " "\"AMALINUX\" : $(aws ec2 describe-images --region $region --filters "Name=owner-id,Values=137112412989" "Name=description,Values=$prefix_str*x86_64 HVM GP2" "Name=virtualization-type,Values=hvm" "Name=root-device-type,Values=ebs" "Name=state,Values=available"|jq -c '.Images[] | {ImageId, Name, Description,VirtualizationType,State}'|jq .ImageId|head -n $ami_number) "#$prefix_str"
     ) 
 done
